5e9269f17a9ec849323c79bdc5be293fa24e0d3d
OwenTill545 <115004897+OwenTill545@users.noreply.github.com>
2023-03-02 08:37:05 -0500

    More Implementation

--- GDS_126/GDS_126_Engine/GDS_126_Engine/js/GameStates/level1.js
+++ GDS_126/GDS_126_Engine/GDS_126_Engine/js/GameStates/level1.js
@@ -57,7 +57,7 @@ levelItems.add([caveBack.grid, ground, plat, cave.grid]);
 
 //Very back background
 var sky = new GameObject({width:canvas.width, height:canvas.height, color:"cyan"})
-sky.img.src = `images/sky.png`
+sky.img.src = `images/GameBackground.png`
 
 /*
  	//Not used, unless you want a 4th level of paralax
@@ -67,7 +67,7 @@ sky.img.src = `images/sky.png`
 
 //repeating background
 var rbg = new GameObject({x:level.x, y:level.y, width:1024, height:512})
-rbg.img.src=`images/hills.png`
+rbg.img.src=`images/MiddleGround.png`
 
 //middleground
 var bg = new GameObject({x:level.x,y:level.y, width:canvas.width*4, height:canvas.height})
