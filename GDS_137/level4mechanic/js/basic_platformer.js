65787b9926f4a9da3bf5804cea09a6e45489be4f
OwenTill545 <115004897+OwenTill545@users.noreply.github.com>
2023-04-27 10:35:41 -0400

    gun aim

---
+++ GDS_137/level4mechanic/js/basic_platformer.js
@@ -0,0 +1,199 @@
+//Declare my variables
+
+var canvas;
+var context;
+var timer;
+var interval;
+var player;
+var gun;
+
+
+
+	canvas = document.getElementById("canvas");
+	context = canvas.getContext("2d");	
+
+	player = new GameObject({x:150});
+	gun = new GameObject();
+
+	gun.width = 60;
+	gun.height = 20;
+	gun.x = player.x + 50;
+	gun.y = player.y - 10;
+	gun.color = "black";
+
+	var bullets = [];
+	
+
+	platform0 = new GameObject();
+		platform0.width = 150;
+		platform0.x = platform0.width/2;
+		platform0.y = player.y +player.height/2 + platform0.height/2;
+		platform0.color = "#66ff33";
+		
+	
+	platform1 = new GameObject();
+		platform1.width = 575;
+		platform1.x = canvas.width -platform1.width/2;
+		platform1.y = player.y +player.height/2 + platform1.height/2;
+		platform1.color = "#66ff33";
+		
+	
+
+	var fX = .85;
+	var fY = .97;
+	
+	var gravity = 1;
+
+	interval = 1000/60;
+	timer = setInterval(animate, interval);
+
+function animate()
+{
+	
+	context.clearRect(0,0,canvas.width, canvas.height);	
+
+	if(w && player.canJump && player.vy ==0)
+	{
+		player.canJump = false;
+		player.vy += player.jumpHeight;
+		gun.vy += player.jumpHeight;
+	}
+
+	if(a)
+	{
+		player.vx += -player.ax * player.force;
+		gun.x = player.x - 50;
+		gun.vx += -player.ax * player.force;
+
+	}
+	if(d)
+	{
+		player.vx += player.ax * player.force;
+		gun.x = player.x + 50;
+		gun.vx += player.ax * player.force;
+	}
+	if (!upArrow)
+	{
+		gun.angle = 0;
+	}
+	if (!downArrow)
+	{
+		gun.angle = 0;
+	}
+	if(upArrow && gun.x == player.x + 50)
+	{
+		gun.angle = -45;
+	}
+	if(downArrow && gun.x == player.x + 50)
+	{
+		gun.angle = 45;
+	}
+	if(upArrow && gun.x == player.x - 50)
+	{
+		gun.angle = 45;
+	}
+	if(downArrow && gun.x == player.x - 50)
+	{
+		gun.angle = -45;
+	}
+	
+
+	player.vx *= fX;
+	gun.vx *= fX;
+	player.vy *= fY;
+	gun.vy *= fY;
+	
+	player.vy += gravity;
+	gun.vy += gravity;
+	
+	player.x += Math.round(player.vx);
+	gun.x += Math.round(player.vx);
+	player.y += Math.round(player.vy);
+	gun.y += Math.round(player.vy);
+	
+
+	while(platform0.hitTestPoint(player.bottom()) && player.vy >=0)
+	{
+		player.y--;
+		gun.y--;
+		player.vy = 0;
+		gun.vy = 0;
+		player.canJump = true;
+	}
+	while(platform0.hitTestPoint(player.left()) && player.vx <=0)
+	{
+		player.x++;
+		gun.x++;
+		player.vx = 0;
+		gun.vx = 0;
+	}
+	while(platform0.hitTestPoint(player.right()) && player.vx >=0)
+	{
+		player.x--;
+		gun.x--;
+		player.vx = 0;
+		gun.vx = 0;
+	}
+	
+	while(platform1.hitTestPoint(player.bottom()) && player.vy >=0)
+	{
+		player.y--;
+		gun.y--;
+		player.vy = 0;
+		gun.vy = 0;
+		player.canJump = true;
+	}
+	while(platform1.hitTestPoint(player.left()) && player.vx <=0)
+	{
+		player.x++;
+		gun.x++;
+		player.vx = 0;
+		gun.vx = 0;
+	}
+	while(platform1.hitTestPoint(player.right()) && player.vx >=0)
+	{
+		player.x--;
+		gun.x--;
+		player.vx = 0;
+		gun.vx = 0;
+	}
+	
+	
+	//---------Objective: Jump the gap to get the blue pearl----------------------------------------------------------------------------------------------------
+	//---------Call the players drawDebug() function to see where his current hitpoints are
+	//---------Change the objects in the hitTestPoint function so that the player has a hitpoint on his bottom left and bottom right.
+	//---------You will need to manually draw these new points if you want to see them for debugging purposes.
+	
+	/*-------------------------------------------------------------------------------------------------------------------------------------
+				NOTE: An unnecessary but great alternative to simply changing the object literals in the hitTestPoint 
+				would be to create actual bottomRight() and bottomLeft() methods in your GameObject. However this is not necessary to complete this level. 
+				You simply need to change the values of the x and y properties of the object literals listed below to find the solution.
+	*/
+
+	while(platform0.hitTestPoint({x:player.left().x, y:player.bottom().y}) && player.vy >=0)
+	{
+		player.y--;
+		gun.y--;
+		player.vy = 0;
+		gun.vy = 0;
+		player.canJump = true;
+	}
+	
+	while(platform1.hitTestPoint({x:player.right().x, y:player.bottom().y}) && player.vy >=0)
+	{
+		player.y--;
+		gun.y--;
+		player.vy = 0;
+		gun.vy = 0;
+		player.canJump = true;
+	}
+	
+
+	
+	platform0.drawRect();
+	platform1.drawRect();
+	gun.drawRect();
+	player.drawRect();
+	
+}
+
---
+++ GDS_137/level4mechanic/js/controls.js
@@ -0,0 +1,80 @@
+var w = false;
+var a = false;
+var s = false;
+var d = false;
+var upArrow = false;
+var downArrow = false;
+var space = false;
+
+document.addEventListener("keydown", press);
+document.addEventListener("keyup", release);
+
+function press(e)
+{
+	//---This logs key codes into the browser's console.
+	console.log(e.keyCode);
+	
+	if(e.keyCode == 87)
+	{
+		w = true;
+	}
+	if(e.keyCode == 65)
+	{
+		a = true;
+	}
+	if(e.keyCode == 83)
+	{
+		s = true;
+	}
+	if(e.keyCode == 68)
+	{
+		d = true;
+	}
+	if(e.keyCode == 38)
+	{
+		upArrow = true;
+	}
+	if(e.keyCode == 40)
+	{
+		downArrow = true;
+	}
+	if(e.keyCode == 32)
+	{
+		space = true;
+	}
+}
+
+function release(e)
+{
+	//---This logs key codes into the browser's console.
+	//console.log(e.keyCode);
+	
+	if(e.keyCode == 87)
+	{
+		w = false;
+	}
+	if(e.keyCode == 65)
+	{
+		a = false;
+	}
+	if(e.keyCode == 83)
+	{
+		s = false;
+	}
+	if(e.keyCode == 68)
+	{
+		d = false;
+	}
+	if(e.keyCode == 38)
+	{
+		upArrow = false;
+	}
+	if(e.keyCode == 40)
+	{
+		downArrow = false;
+	}
+	if(e.keyCode == 32)
+	{
+		space = false;
+	}
+}
